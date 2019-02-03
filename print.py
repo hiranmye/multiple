6000b562e7d277c29c0fd281d2da30b7fde47560
hiranmye <46378734+hiranmye@users.noreply.github.com>
2019-02-03 12:17:50 +0530

    Create print.py

---
+++ print.py
@@ -0,0 +1,9 @@
+
+num = int(input(""))
+
+# To take input from the user
+# num = int(input("Display multiplication table of? "))
+
+# use for loop to iterate 10 times
+for i in range(1, 6):
+   print(num*i),
